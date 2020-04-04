17cca6a6260514a4865137a864ee16999d4cec0a
xuqinkun <15528498250@163.com>
2020-04-04 11:14:19 +0800

    Exc_042_CatchWater.java

--- src/leetcode/medium/arrays/Exc_300_LargestIncreasingSequence.java
+++ src/leetcode/medium/arrays/Exc_300_LargestIncreasingSequence.java
@@ -5,28 +5,31 @@ public class Exc_300_LargestIncreasingSequence {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length, max = 0;
         if (n == 0) return max;
-        int [] dp = new int[n];
-
-        dp[0] = 1;
+        int [] dp = new int[n + 1];
+        dp[1] = nums[0];
+        int len = 1;
         for (int i = 1; i < n; i++) {
-            max = 1;
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    max = Math.max(max, dp[j] + 1);
+            if (dp[len] < nums[i]) dp[++len] = nums[i];
+            else {
+                int l = 1, r = len, mid = 0, pos = 0;
+                while (l <= r) {
+                    mid = (l + r) >> 1;
+                    if (dp[mid] < nums[i]) {
+                        pos = mid;
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
                 }
+                dp[pos + 1] = nums[i];
             }
-            dp[i] = max;
         }
-        max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, dp[i]);
-        }
-        return max;
+        return len;
     }
 
     public static void main(String[] args) {
         Exc_300_LargestIncreasingSequence lis = new Exc_300_LargestIncreasingSequence();
-        int[] a = {1,3,6,7,9,4,10,5,6};
+        int[] a = {1,3,6,7,9,4,5,6};
         System.out.println(lis.lengthOfLIS(a));
     }
 }
