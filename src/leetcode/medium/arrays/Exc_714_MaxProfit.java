ca38d79bff7164c4f1fef6302a125bdf1120242b
xuqinkun <xu_qinkun@163.com>
2021-11-17 09:12:14 +0800

    股票最大获利空间优化

--- src/leetcode/medium/arrays/Exc_714_MaxProfit.java
+++ src/leetcode/medium/arrays/Exc_714_MaxProfit.java
@@ -21,12 +21,11 @@ public class Exc_714_MaxProfit {
      */
     public int maxProfit(int[] prices, int fee) {
         int len = prices.length;
-        int[][] dp = new int[len][2];
-        dp[0][0] = -prices[0];
+        int maxProfitWithStock = -prices[0], maxProfitWithOutStock = 0;
         for (int i = 1; i < len; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] - prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i] - fee);
+            maxProfitWithStock = Math.max(maxProfitWithStock, maxProfitWithOutStock - prices[i]);
+            maxProfitWithOutStock = Math.max(maxProfitWithOutStock, maxProfitWithStock + prices[i] - fee);
         }
-        return dp[len - 1][1];
+        return maxProfitWithOutStock;
     }
 }
